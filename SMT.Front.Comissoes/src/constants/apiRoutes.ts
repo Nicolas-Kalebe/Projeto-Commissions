0f6abd98b4891f98951c30926db761002347aaae
JhonathanOliveiraa <jhonathanoliveiraa@users.noreply.github.com>
2026-01-27 10:52:36 -0300

    localhost back

--- SMT.Front.Comissoes/src/constants/apiRoutes.ts
+++ SMT.Front.Comissoes/src/constants/apiRoutes.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:5260"
+  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:5100"
 
 export const API_ROUTES = {
   Auth: {
