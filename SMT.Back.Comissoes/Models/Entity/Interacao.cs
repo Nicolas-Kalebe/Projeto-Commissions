be40cc218caec0cda95eed34990ebbbce91fcb17
JhonathanOliveiraa <jhonathanoliveiraa@users.noreply.github.com>
2026-02-01 17:19:44 -0300

    Feat - Adicionando interações dentro do sistema

---
+++ SMT.Back.Comissoes/Models/Entity/Interacao.cs
@@ -0,0 +1,22 @@
+﻿using SMT.Back.Comissoes.Models.Enum;
+
+namespace SMT.Back.Comissoes.Models.Entity
+{
+    public class Interacao
+    {
+        public int Id { get; set; }
+
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+        public TipoInteracaoEnum TipoInteracao { get; set; }
+        // Like, Favorito
+
+        public TipoAlvoInteracaoEnum TipoAlvoInteracao { get; set; }
+        // PortfolioItem, Post, PerfilArtista
+
+        public int AlvoId { get; set; }
+
+        public DateTime DataDeCriacao { get; set; }
+    }
+}
---
+++ SMT.Back.Comissoes/Models/Enum/TipoAlvoInteracaoEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace SMT.Back.Comissoes.Models.Enum
+{
+    public enum TipoAlvoInteracaoEnum
+    {
+        PortfolioItem = 1,
+        Post = 2,
+        PerfilArtista = 3
+    }
+}
